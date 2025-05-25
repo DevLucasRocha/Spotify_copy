c2bdaf06410b1fd022f156472d3a7df60475cb1f
Lucas Rocha <lucas_santos239@outlook.com>
2025-05-24 21:53:44 -0300

    Update Home.jsx

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchArtists, fetchSongs } from "../api/api";
+import { fetchArtists, fetchSongs } from "../../api/api";
 import ItemList from "../components/ItemList";
 
 const Home = () => {
