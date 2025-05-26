f5cc814248c015c2a7829ba41ecddc1f679bff4a
Lucas Rocha <lucas_santos239@outlook.com>
2025-05-25 22:16:34 -0300

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Artists from "./pages/Artists";
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/songs" element={<Songs />} />
         <Route path="/song/:id" element={<Song />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
