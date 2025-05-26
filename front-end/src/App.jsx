31a424c436457543293f4c2edb089dbe6c0b5dcd
Lucas Rocha <lucas_santos239@outlook.com>
2025-05-26 12:15:15 -0300

    update app.jsx otimization

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Artists from "./pages/Artists";
-import Artist from "./pages/Artist";
-import Songs from "./pages/Songs";
-import Song from "./pages/Song";
+
+const Home = lazy(() => import("./pages/Home"));
+const Artists = lazy(() => import("./pages/Artists"));
+const Artist = lazy(() => import("./pages/Artist"));
+const Songs = lazy(() => import("./pages/Songs"));
+const Song = lazy(() => import("./pages/Song"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/artists" element={<Artists />} />
-        <Route path="/artist/:id" element={<Artist />} />
-        <Route path="/songs" element={<Songs />} />
-        <Route path="/song/:id" element={<Song />} />
-      </Routes>
+      <Suspense fallback={<p style={{textAlign: "center"}}>Carregando...</p>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/artist/:id" element={<Artist />} />
+          <Route path="/songs" element={<Songs />} />
+          <Route path="/song/:id" element={<Song />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </BrowserRouter>
   );
