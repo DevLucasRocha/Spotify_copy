2b4f0cc4eb3237ebb2e09e287dea5fa971f5e3bc
Lucas Rocha <lucas_santos239@outlook.com>
2025-05-25 22:23:50 -0300

    Update server.js

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -11,7 +11,9 @@ import { db } from "./connect.js";
 const app = express();
 const PORT = 3001;
 
-app.use(cors({ origin: "https://project-spotifyweb.netlify.app/" }));
+app.use(cors({ origin: 
+    "https://spotify-web-copy.netlify.app",
+    "https://project-spotifyweb.netlify.app" }));
 
 app.get("/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
